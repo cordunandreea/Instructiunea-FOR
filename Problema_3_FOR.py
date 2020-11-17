726a1d277cf899cb9eaae5c9ab4f4d737646da55
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-11-17 09:46:02 +0300

    Update Problema_3_FOR.py

--- Problema_3_FOR.py
+++ Problema_3_FOR.py
@@ -1,3 +1,4 @@
+#Utilizînd ciclul FOR elaboraţi un program care afişează toate numerele pare, pentru intervalul de la 1 la n (n-este citit de la tastatură).
 n=int(input('n='))
 for i in range (0,n,2):
     print(i, end=',')
