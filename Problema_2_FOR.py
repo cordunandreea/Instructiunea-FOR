973260146c8be64b451f9d95ef145e621f3111f6
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-11-17 09:45:17 +0300

    Update Problema_2_FOR.py

--- Problema_2_FOR.py
+++ Problema_2_FOR.py
@@ -1,3 +1,3 @@
 #Utilizînd ciclul FOR elaboraţi un program care afişează primele 10 numere naturale şi păstrarea lor în ordine descrescătoare.
-for i in (9,8,7,6,5,4,3,2,1,0):
-    print(i)
+for i in reversed(range(0,10)):
+    print(i, end=' ')
