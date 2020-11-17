e02325cc2ce27e672d798b39ab5d0d02aab1a286
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-11-17 09:50:58 +0300

    Update Problema_5_FOR.py

--- Problema_5_FOR.py
+++ Problema_5_FOR.py
@@ -1,6 +1,7 @@
+#Utilizînd ciclul FOR elaboraţi un program care să calculeze suma numerelor de la 1 la n, care se împart la 3 şi 5 pentru oricare n întrodus de la tastatură.
 n=int(input('n='))
 a=0
 for i in range(1,n):
-    if i%3==0 or i%5==0:
+    if i%3==0 and i%5==0:
         a+=i
 print(a)
