fdc6f9d310e4813b7e0cfb0fee64341f1e0f7c95
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-11-17 09:48:46 +0300

    Update Problema_4_FOR.py

--- Problema_4_FOR.py
+++ Problema_4_FOR.py
@@ -1,5 +1,6 @@
+#Utilizînd ciclul FOR elaboraţi un program care afişează toate numerele impare, pentru intervalul de la a la b (a și b se citesc de la tastatură).
 a=int(input('a='))
 b=int(input('b='))
-for i in range(a,b):
-    if i%2==1:
+for i in range(a,b,1):
+    if i%2!=0:
         print(i)
